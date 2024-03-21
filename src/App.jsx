85578829e9c2fc640bc59f44298c656c16c92f14
xsjark <xsjark@gmail.com>
2024-03-21 13:43:41 +0900

    done

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://derp-env.eba-apniuz4q.us-east-1.elasticbeanstalk.com/api/data');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Express:</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
